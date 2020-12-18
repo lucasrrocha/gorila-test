096b69b6c93de6b8b716be78e6fcab06d143342d
Lucas Rocha <lucas_r.rocha@outlook.com>
2020-12-18 11:23:10 -0300

    Modified components

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -4,20 +4,20 @@ export const Container = styled.div``;
 
 export const Button = styled.button`
   width: 100%;
-  height: 5.6rem;
+  height: 2.5rem;
   background: var(--color-primary);
   color: var(--color-button-text);
   outline: 0;
   border: 0;
   border-radius: 0.8rem;
   cursor: pointer;
-  font: 700 1.6rem Archivo;
+  font: 700 1.125rem Archivo;
   display: flex;
   align-items: center;
   justify-content: center;
   text-decoration: none;
   transition: background-color 0.2s;
-  margin-top: 3.2rem;
+  margin-top: 1.125rem;
 
   &&:hover {
     background: var(--color-primary-dark);
--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -2,23 +2,23 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   position: relative;
-  margin-top: ${props => (props.type === 'password' ? '2.5rem' : '0')};
+  margin-top: ${props => (props.type === 'password' ? '1.5rem' : '0')};
 `;
 
 export const Label = styled.label`
-  font-size: 1.5rem;
+  font-size: 1.125rem;
 `;
 
 export const Input = styled.input.attrs(props => ({
   type: props.type
 }))`
   width: 100%;
-  height: 5.6rem;
-  margin-top: 0.8rem;
+  height: 2.5rem;
+  margin-top: 0.4rem;
   border-radius: 0.8rem;
   background-color: var(--color-input-background);
   border: 1px solid var(--color-line-in-white);
   outline: 0;
-  padding: 0 1.6rem;
-  font: 1.6rem Archivo;
+  padding: 0 1.125rem;
+  font: 1rem Archivo;
 `;
